8670634e9233494b9ada9de77c61997b1f0a9a58
Monsieur198 <mons198x@gmail.com>
2021-04-04 17:28:25 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// const numbers = [54, 20, 150, -150, 40, 65, -44];
+
+// const names = ['vladiMir', 'IrIna', 'andRey', 'Mark', 'NaTaLy', 'Katya'];
+
+// const mix = ['Tram', 323, 'PaM', -32, null, '15', 433, 'pam'];
+
+// const badNum = [54, 20, 75, -544, -45, 'hi', 20, 69, 8];
+
+
+// let sum = 0;
+
+// for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+// }
+
+// // console.log(sum);
+
+// let sum2 = numbers.reduce(function(acc, item, i, arr) {
+//     console.table({acc, item});
+//     return acc + item;
+// }, 0);
+
+// console.log(sum2);
+
+const user = 'Ivan';
+let age = '18';
+
+console.log(` yo
+Привет ${user},
+Сейчас тебе ${age++} лет,
+а скоро будет уже ${age}..
+yo `);
