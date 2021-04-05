37eb50638664e1a9c8e5eb797551eac52f398a49
Monsieur198 <mons198x@gmail.com>
2021-04-06 01:23:25 +0300

    1st homework

--- js/script.js
+++ js/script.js
@@ -1,34 +1,19 @@
 'use strict';
 
-// const numbers = [54, 20, 150, -150, 40, 65, -44];
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// const names = ['vladiMir', 'IrIna', 'andRey', 'Mark', 'NaTaLy', 'Katya'];
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
 
-// const mix = ['Tram', 323, 'PaM', -32, null, '15', 433, 'pam'];
+for (let i = 0; i < 2; i++) {
+    let movie = prompt('Один из последних просмотренных фильмов?', '');
+    let rating = prompt('На сколько оцените его?', '');
+    personalMovieDB.movies[movie] = rating;
+}
 
-// const badNum = [54, 20, 75, -544, -45, 'hi', 20, 69, 8];
-
-
-// let sum = 0;
-
-// for (let i = 0; i < numbers.length; i++) {
-//     sum += numbers[i];
-// }
-
-// // console.log(sum);
-
-// let sum2 = numbers.reduce(function(acc, item, i, arr) {
-//     console.table({acc, item});
-//     return acc + item;
-// }, 0);
-
-// console.log(sum2);
-
-const user = 'Ivan';
-let age = '18';
-
-console.log(` yo
-Привет ${user},
-Сейчас тебе ${age++} лет,
-а скоро будет уже ${age}..
-yo `);
+console.log(personalMovieDB.movies);
