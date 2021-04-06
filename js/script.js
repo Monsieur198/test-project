b8ab95a149bd24e47317c1ee34ff0228fcfe8158
Monsieur198 <mons198x@gmail.com>
2021-04-06 16:46:25 +0300

    Added style + second homework

--- js/script.js
+++ js/script.js
@@ -10,10 +10,29 @@ const personalMovieDB = {
     private: false
 };
 
+switch (true) {
+    case personalMovieDB.count < 10:
+        console.log('Просмотрено довольно мало фильмов');
+        break;
+    case personalMovieDB.count >= 10 && personalMovieDB.count <= 30:
+        console.log('Вы - классический зритель');
+        break;
+    case personalMovieDB.count > 30:
+        console.log('Вы - киноман!');    
+        break;
+}
+
 for (let i = 0; i < 2; i++) {
-    let movie = prompt('Один из последних просмотренных фильмов?', '');
-    let rating = prompt('На сколько оцените его?', '');
+    do {
+        var movie = prompt('Один из последних просмотренных фильмов?', '');
+    } while (!movie || movie == ' ' || movie.length > 50);
+    
+    do {
+        var rating = prompt('На сколько оцените его?', '');
+    } while (!rating || rating == ' ' || rating.length > 50);
+
     personalMovieDB.movies[movie] = rating;
 }
 
 console.log(personalMovieDB.movies);
+
